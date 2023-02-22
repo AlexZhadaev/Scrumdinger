d07c20eb827bd5d7cd80960169e842c618359c25
AlexZhadaev <pvt.vatnik@gmail.com>
2023-02-22 20:32:23 +0300

    create a data store

---
+++ Scrumdinger/Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Жадаев Алексей on 22.02.2023.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("scrums.data")
+    }
+}
